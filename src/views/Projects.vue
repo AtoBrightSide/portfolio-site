72be643b974944d2caf9902ad32c2e452f76f2be
AtoBrightSide <meseleabel99@gmail.com>
2022-09-11 09:51:14 +0300

    fix: add info on projects page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,6 +3,9 @@
         <div class="text-6xl text-red-700/80 animate-pulse text-center">
             Currently in the works!!
         </div>
+        <div class=" mt-11 text-6xl text-red-700/80 animate-pulse text-center">
+            Stay Tuned
+        </div>
     </section>
 </template>
 
