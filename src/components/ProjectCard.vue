f43b2e84bde142bc24c2c36de3cb8dd4f4de1ae6
Abel Mesele <se.abel.mesele@gmail.com>
2022-09-29 21:54:20 +0300

    added projects

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,17 +1,37 @@
 <template>
-  <div class="flex flex-col bg-gray-700 rounded-md p-4 hover:bg-gray-800 hover:shadow-lg hover:shadow-gray-700">
-    <img class="w-full h-1/3 object-contain" src="" alt="rateeat logo">
-    <div class="text-2xl text-white">
-        RateEat
+  <div
+    class="flex flex-col rounded-md p-4 drop-shadow-xl"
+  >
+    <img
+      class="w-full h-1/3 object-contain"
+      src="../assets/laptop-gf2aa9359c_1280.png"
+      alt="project_image"
+    />
+    <div class="text-2xl text-white text-center my-5">
+      {{ title }}
     </div>
     <p class="text-slate-50/80">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia reiciendis accusamus soluta ullam exercitationem non tempore magni quam, suscipit voluptates voluptatem eos nisi similique possimus necessitatibus reprehenderit quaerat porro praesentium.
+      {{ projectDesc }}
     </p>
+    <div class="flex text-white">
+      <div class="font-bold">Technologies Used:</div>
+      <div v-for="tech in tech_stack" :key="tech" class="flex">
+        <p class="mx-1">{{ tech }},</p>
+      </div>
+    </div>
+    <div class="flex w-full justify-center gap-2 mt-3">
+      <a :href="info.demo_link">
+        <Button :info="info"/>
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
+import Button from "./Button.vue";
 export default {
-    name: "ProjectCard"
+  name: "ProjectCard",
+  props: ["title", "projectImage", "projectDesc", "tech_stack", "info"],
+  components: { Button },
 };
 </script>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,22 +1,84 @@
 <template>
-    <section class="container mx-auto">
-        <div class="text-4xl text-white text-center bg-gray-700">
-            Some Passion Projects
-        </div>
-        <div class="flex flex-col md:grid gap-4 md:grid-cols-2 lg:grid-cols-3 mt-3">
-            <ProjectCard />
-            <div class="bg-indigo-600">2</div>
-            <div class="bg-indigo-600">3</div>
-            <div class="bg-indigo-600">4</div>
-            <div class="bg-indigo-600">5</div>
-        </div>
-    </section>
+  <section class="h-fit container mx-auto space-y-11">
+    <div class="text-white text-center flex flex-col space-y-4">
+      <div class="text-5xl font-bold">
+        Previous <span class="text-hotmag">Work</span>
+      </div>
+      <div class="text-xl">
+        Here are some <span class="text-hotmag">projects</span> of mine
+      </div>
+    </div>
+    <div class="flex flex-col md:grid gap-4 md:grid-cols-2 lg:grid-cols-3 mt-3">
+      <div v-for="project in projects" :key="project.title">
+        <ProjectCard
+          :title="project.title"
+          :projectDesc="project.projectDesc"
+          :projectImage="project.projectImage"
+          :tech_stack="project.tech_stack"
+          :info="project.info"
+        />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import ProjectCard from '../components/ProjectCard.vue';
-    export default {
-    name: "Projects",
-    components: { ProjectCard }
-}
+import ProjectCard from "../components/ProjectCard.vue";
+export default {
+  name: "Projects",
+  components: { ProjectCard },
+  data: () => {
+    return {
+      projects: [
+        {
+          title: "RateEat",
+          projectDesc:
+            "A platform that allows users to give and consume item specific reviews and show growth points of businesses.",
+          projectImage: "../assets/photo_2022-07-17_20-35-23-modified.png",
+          tech_stack: ["Vue", "Nuxt", "Express", "MongoDB"],
+          info: {
+            product: "Website",
+            demo_link: "https://rateeat.app",
+            github: false,
+          },
+        },
+        {
+          title: "getz Cars",
+          projectDesc:
+            "A mobile friendly web application that allows users to look for cars according to their preference.",
+          projectImage: "Something",
+          tech_stack: ["React", "Express", "MongoDB"],
+          info: {
+            product: "Website",
+            demo_link: "https://getzcarsell.com",
+            github: false,
+          },
+        },
+        {
+          title: "The Joker",
+          projectDesc:
+            "An android mobile application that fetches users from a thrid party API, and also allows users to post jokes too.",
+          projectImage: "Something",
+          tech_stack: ["Flutter", "Dart"],
+          info: {
+            product: "Mobile",
+            demo_link: "https://github.com/AtoBrightSide/flutter-jokes-app",
+            github: true,
+          },
+        },
+        {
+          title: "Lyrics API",
+          projectDesc: "A simple API that has ",
+          projectImage: "Something",
+          tech_stack: ["Python", "Flask"],
+          info: {
+            product: "Web App",
+            demo_link: "https://github.com/AtoBrightSide/lyrics-api",
+            github: true,
+          },
+        },
+      ],
+    };
+  },
+};
 </script>
