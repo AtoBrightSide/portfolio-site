02621d9fda130c4ac38123483f2d4766e11ef784
Abel Mesele <meseleabel99@gmail.com>
2022-10-03 00:54:38 +0300

    some refactoring

---
+++ src/components/Herosection.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-start mx-auto font-body p-4 md:p-20 md:space-x-20 md:flex md:flex-row md:justify-center">
+    <div class="relative h-36 w-36 md:h-96 md:w-96">
+      <img src="../assets/photo_2022-07-17_20-35-23-modified.png" alt="its_me"
+        class="h-full w-full object-contain rounded-full relative border-1" />
+    </div>
+    <div class="w-full flex flex-col items-center content-center md:flex md:flex-row md:justify-between md:mt-12">
+      <div class="w-fit flex-col p-5 md:p-0">
+        <div class="text-[#5C7356] font-thin md:text-lg">
+          Hi, the name is
+          <div class="text-4xl font-semibold text-[#5C7356] md:font-thin md:text-6xl">
+            Abel Mesele
+          </div>
+        </div>
+        <div class="flex text-4xl w-full font-semibold md:font-thin md:text-6xl">
+          <Type />
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script scoped>
+import Type from "../components/Type.vue";
+export default {
+  name: "Herosection",
+  components: { Type },
+};
+</script>
---
+++ src/components/Projects.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class="h-fit container my-5 mx-auto space-y-11">
+
+    <div class="flex justify-center text-5xl text-[#5B7355] font-bold">
+      Previous <span class="text-[#9DBF84]">Work</span>
+    </div>
+
+    <div class="flex flex-col md:grid gap-4 md:grid-cols-2 lg:grid-cols-3 mt-3">
+      <div v-for="project in projects" :key="project.title">
+        <ProjectCard :title="project.title" :projectDesc="project.projectDesc" :projectImage="project.projectImage"
+          :tech_stack="project.tech_stack" :info="project.info" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ProjectCard from "../components/ProjectCard.vue";
+export default {
+  name: "Projects",
+  components: { ProjectCard },
+  data: () => {
+    return {
+      projects: [
+        {
+          title: "RateEat",
+          projectDesc:
+            "A platform that allows users to give and consume item specific reviews and show growth points of businesses.",
+          projectImage: "../assets/photo_2022-07-17_20-35-23-modified.png",
+          tech_stack: ["Next", "Typescript", "Express", "MongoDB"],
+          info: {
+            product: "Website",
+            demo_link: "https://rateeat.app",
+            github: false,
+          },
+        },
+        {
+          title: "getz Cars",
+          projectDesc:
+            "A mobile friendly web application that allows users to look for cars according to their preference.",
+          projectImage: "Something",
+          tech_stack: ["React", "Typescript", "Express", "MongoDB"],
+          info: {
+            product: "Website",
+            demo_link: "https://getzcarsell.com",
+            github: false,
+          },
+        },
+        {
+          title: "The Joker",
+          projectDesc:
+            "Explore a variety of jokes by type, save your favorites, and get a daily laugh with our fun and easy-to-use joke app.",
+          projectImage: "Something",
+          tech_stack: ["Flutter", "Dart",],
+          info: {
+            product: "Mobile",
+            demo_link: "https://github.com/AtoBrightSide/flutter-jokes-app",
+            github: true,
+          },
+        },
+        {
+          title: "Lyrics API",
+          projectDesc: "The lyrics API allows music lovers to explore, upload, and like song lyrics, with full CRUD functionality and user registration ",
+          projectImage: "Something",
+          tech_stack: ["Python", "Flask", "Postgres"],
+          info: {
+            product: "Web App",
+            demo_link: "https://github.com/AtoBrightSide/lyrics-api",
+            github: true,
+          },
+        },
+      ],
+    };
+  },
+};
+</script>
