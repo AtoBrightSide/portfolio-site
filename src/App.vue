117f3ebb97831cf248940a6588b910d2502a82d8
Abel Mesele <meseleabel99@gmail.com>
2022-10-03 02:13:28 +0300

    refactor app

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <div
-    id="top"
-    class="flex-col bg-bgPattern w-full p-0 m-0 scroll-smooth font-body"
-  >
-    <Navbar class="sticky top-0" />
-    <div>
-      <router-view />
-    </div>
-    <Footer class="bottom-0 mt-5"/>
+  <div id="top" class="flex-col bg-[#D9D8D7] text-[#5C7356] w-full p-0 m-0 scroll-smooth font-body">
+    <Navbar />
+    <Herosection id="herosection"/>
+    <About id="about"/>
+    <Projects id="projects"/>
+    <Connect />
+    <Footer class="bottom-0 mt-5" />
   </div>
 </template>
+
 <script scoped>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
-import Herosection from "./views/Herosection.vue";
-import About from "./views/About.vue";
+import About from "./components/About.vue";
 import Connect from "./components/Connect.vue";
+import Herosection from "./components/Herosection.vue";
+import Projects from "./components/Projects.vue"
 export default {
   components: {
     Navbar,
     Footer,
     Herosection,
     About,
+    Projects,
     Connect,
   },
 };
