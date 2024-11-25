6bd8224565cd3bdf9ee68960e6f04000a6828ec2
Abel Mesele <meseleabel99@gmail.com>
2024-11-25 21:42:24 +0300

    Add Finishing touches

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -6,10 +6,10 @@ export const Introduction = () => {
         <div ref={ref} className={`w-full flex flex-col md:flex-row md:justify-around items-start p-10 gap-y-2 ${inView ? 'animate__animated animate__fadeIn' : ''}`}>
             <div className="flex flex-col gap-y-2 md:items-start">
                 <div className="text-3xl md:text-5xl font-bold md:font-semibold text-center mt-5 md:mb-5">Allow me to introduce myself</div>
-                <div className="">A Software Engineer</div>
-                <div className="">Proficient in Python, JavaScript and TypeScript</div>
-                <div className="">Built projects in React, Next, Express, Flask</div>
-                <div className="">Enjoy reading books, watching movies, and sacrificing pieces in chess!</div>
+                <div className="text-lg">A Software Engineer</div>
+                <div className="text-lg">Proficient in Python, JavaScript and TypeScript</div>
+                <div className="text-lg">Built projects in React, Next, Express, Flask</div>
+                <div className="text-lg">Enjoy reading books, watching movies, and sacrificing pieces in chess!</div>
             </div>
             <img src="/6170426_3054594.svg" alt="portrait" className="w-56 h-56 md:w-96 md:h-96 self-center" />
         </div>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import { DotsHorizontalIcon, GitHubLogoIcon, Link1Icon } from "@radix-ui/react-icons"
 import { useState } from "react"
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import { useInView } from "react-intersection-observer";
 
 export interface ProjectCardProps {
@@ -14,18 +15,28 @@ export interface ProjectCardProps {
 export const ProjectCard = ({ project }: { project: ProjectCardProps }) => {
     const [showMore, setShowMore] = useState(false);
     const { ref, inView } = useInView();
+
+    const Icon = () => {
+        return (
+            <div className="w-5 h-5 hover:cursor-pointer hover:opacity-50" onClick={_ => setShowMore(!showMore)}>
+                {showMore ? <FaAngleUp /> : <FaAngleDown />}
+            </div>
+        );
+    }
+
+
     return (
-        <div ref={ref} className={`w-96 md:w-[500px] h-96 ${showMore ? 'h-fit' : ''} md:h-[600px] flex flex-col justify-between items-center md:mx-auto p-5 md:p-2 ${inView ? 'animate__animated animate__fadeIn' : ''}`}>
+        <div ref={ref} className={`w-96 md:w-[500px] h-96 ${showMore ? 'h-fit' : ''} md:h-[550px] flex flex-col justify-between items-center md:mx-auto p-5 ${inView ? 'animate__animated animate__fadeIn' : ''}`}>
             <img src={project.projectImage} alt="ecommerce_image" className="w-full" />
             <div className="text-center text-2xl">{project.title}</div>
             <div className="flex gap-2">
-                <div className={`w-[90%] h-12 overflow-clip ${showMore ? 'h-fit' : 'overflow-hidden'}`}>{project.projectDesc}</div>
-                <DotsHorizontalIcon className="w-5 h-5 hover:cursor-pointer hover:opacity-50" onClick={_ => setShowMore(!showMore)}></DotsHorizontalIcon>
+                <div className={`w-[90%] h-12 overflow-clip md:text-lg ${showMore ? 'h-fit' : 'overflow-hidden'}`}>{project.projectDesc}</div>
+                <Icon />
             </div>
-            <div className={`w-full h-10 ${showMore ? 'h-fit' : 'overflow-clip'} overflow-y-hidden flex flex-wrap text-start`}>
-                <div className="font-semibold">Tech Stack: </div>
+            <div className={`w-full h-10 md:text-lg ${showMore ? 'h-fit' : 'overflow-clip'} overflow-y-hidden flex flex-wrap text-start gap-x-1`}>
+                <div className="font-semibold mr-2">Tech Stack: </div>
                 {project.tech_stack.map((stack, index) =>
-                    <div key={index}>{stack}, </div>
+                    <div key={index}>{stack}{index === project.tech_stack.length - 1 ? '.' : ','} </div>
                 )}
             </div>
             <div className="w-32 h-8 rounded-md bg-[#5B7355] text-white flex justify-center hover:opacity-80 hover:cursor-pointer transition-all duration-300">
