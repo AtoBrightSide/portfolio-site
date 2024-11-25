e4ccf4a2a9dff21a1d19974dd930c8212c26fc89
Abel Mesele <meseleabel99@gmail.com>
2024-11-25 21:19:00 +0300

    Handle responsiveness

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -68,9 +68,9 @@ export const Herosection = () => {
 
             {/* <div className="hidden md:flex w-96 h-96 absolute bg-red-100 right-32 top-72"></div> */}
 
-            <div className="h-min w-4/5 my-10 md:mt-0 md:w-1/4 flex flex-wrap justify-center gap-x-2 gap-y-5 p-5">
+            <div className="h-min w-4/5 my-10 md:mt-0 md:w-2/5 lg:w-1/4 flex flex-wrap justify-center gap-x-2 gap-y-5 p-5">
                 {ICONS.map((Icon, index) => (
-                    <Icon key={index} className={`w-10 h-10 p-0 opacity-50 hover:opacity-90 transition-all duration-400`}></Icon>
+                    <Icon key={index} className={`w-10 h-10 md:w-12 md:h-12 p-0 opacity-50 hover:opacity-90 transition-all duration-400`}></Icon>
                 ))}
             </div>
 
