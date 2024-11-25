ba4a7039f1caa0fdc1f3cafb78c55d5743930247
Abel Mesele <meseleabel99@gmail.com>
2024-11-25 21:19:17 +0300

    Adjust logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { MixIcon, ReaderIcon, RocketIcon } from "@radix-ui/react-icons";
 export const Navbar = () => {
     return (
         <nav className="bg-[#D9D8D7] flex flex-col md:flex-row md:justify-between md:sticky md:top-0 md:z-10">
-            <div className="text-8xl font-bold text-center mb-5">{'</>'}</div>
+            <div className="text-8xl font-bold text-center mb-5 md:px-5">{'</>'}</div>
             <div className="w-full md:w-1/2 flex md:flex-row justify-around md:items-center">
                 <a href="#introduction" className="flex flex-col items-center hover:opacity-70 hover:cursor-pointer">
                     <RocketIcon className='w-7 h-7'></RocketIcon>
