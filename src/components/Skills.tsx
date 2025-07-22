3955f752a9193d50c4bb83cf6fb39711276b9c23
Abel Mesele <meseleabel99@gmail.com>
2025-07-22 11:29:11 +0300

    feat: add keyframes for skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -34,12 +34,17 @@ const ICONS: IconType[] = [
 ]
 
 export const Skills = () => {
+    const duplicatedIcons = [...ICONS, ...ICONS];
+
     return <section className="max-w-[768px] mx-auto dark:text-corn-silk">
         <h1 className="font-display dark:text-corn-silk/70">Skills</h1>
-        <div className="h-min my-10 md:mt-0 flex flex-wrap justify-center gap-x-2 gap-y-5 p-5">
-            {ICONS.map((Icon, index) => (
-                <Icon key={index} className={`w-10 h-10 md:w-12 md:h-12 p-0 opacity-50 hover:opacity-90 transition-all duration-400`}></Icon>
-            ))}
+        <div
+            className="h-min my-10 md:mt-0 border-r border-l rounded-tl-md rounded-br-md overflow-hidden mask-[linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] flex gap-x-2 gap-y-5 px-2 py-3">
+            <div className="flex gap-1 animate-marquee hover:[animation-play-state:paused] mask-l-from-30%">
+                {duplicatedIcons.map((Icon, index) => (
+                    <Icon key={index} className={`w-10 h-10 md:w-12 md:h-12 p-0 opacity-50 hover:opacity-90 transition-all duration-400}`}></Icon>
+                ))}
+            </div>
         </div>
     </section>
 }
